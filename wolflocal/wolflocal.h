f5a0a7b6de054867ed6b5a059f46c748c03b5057
John Safranek <john@wolfssl.com>
2018-01-10 14:41:09 -0800

    Added changes to wolfLocal header from the customer.

--- wolflocal/wolflocal.h
+++ wolflocal/wolflocal.h
@@ -5,47 +5,49 @@
 #ifndef _WOLFLOCAL_H_
 #define _WOLFLOCAL_H_
 
-int mySeed(unsigned char*, unsigned int);
+extern UCHAR *heap_address;
+extern UINT heap_size;
+
 unsigned int LowResTimer(void);
 
 
-void WolfLocalInit(UCHAR id);
-void WolfLocalTimer(void);
-struct in_addr WolfLocalGetKeySrvAddr(void);
-
-
 typedef struct wolfWrapper_t {
-    UINT streamId;
-    WOLFSSL_CTX* ctx;
-    WOLFSSL* curSsl;
-    WOLFSSL* prevSsl;
-    unsigned short epoch;
-    unsigned short newEpoch;
-    KeyRespPacket_t keyState;
-    UINT keySet;
-    UINT switchKeys;
-    const USHORT* peerIdList;
-    UINT peerIdListSz;
-    unsigned char myId;
-    ULONG groupAddr;
-    unsigned short groupPort;
-    UINT macDropCount;
-    UINT replayDropCount;
-    UINT epochDropCount;
-    NX_UDP_SOCKET realTxSocket;
-    NX_UDP_SOCKET realRxSocket;
-    NX_IP *ip;
-    NX_PACKET_POOL *pool;
-    NX_PACKET *rxPacket;
+	WOLFSSL_CTX* ctx;
+	WOLFSSL* curSsl;
+	WOLFSSL* prevSsl;
+	unsigned short prevEpoch;
+	unsigned short epoch;
+	unsigned short newEpoch;
+	KeyRespPacket_t keyState;
+	UINT keySet;
+	UINT switchKeys;
+	const USHORT* peerIdList;
+	UINT peerIdListSz;
+	unsigned char myId;
+	ULONG groupAddr;
+	unsigned short groupPort;
+	UINT macDropCount;
+	UINT replayDropCount;
+	UINT epochDropCount;
+	NX_UDP_SOCKET realTxSocket;
+	NX_UDP_SOCKET realRxSocket;
+	NX_IP *ip;
+	NX_PACKET_POOL *pool;
+	NX_PACKET *rxPacket;
 } wolfWrapper_t;
 
+void WolfLocalInit(wolfWrapper_t*, UCHAR);
+void WolfLocalTimer(void);
+void WolfLocalRekey(void);
+struct in_addr WolfLocalGetKeySrvAddr(void);
 
-int wolfWrapper_Init(wolfWrapper_t*, UINT, UCHAR, USHORT, ULONG,
-                     const USHORT*, UINT, void*, UINT);
+int wolfWrapper_Init(wolfWrapper_t*, UCHAR, USHORT, ULONG,
+					 const USHORT*, UINT, void*, UINT);
 int wolfWrapper_Update(wolfWrapper_t*);
 int wolfWrapper_Write(wolfWrapper_t*, const void*, int);
+void wolfWrapper_Read_Packet(wolfWrapper_t*, NX_PACKET*, USHORT*, void*, int, int*);
 int wolfWrapper_Read(wolfWrapper_t*, USHORT*, void*, int);
 int wolfWrapper_GetErrorStats(wolfWrapper_t*, unsigned int*,
-                              unsigned int*, unsigned int*);
+							  unsigned int*, unsigned int*);
 
 #endif
